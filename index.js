c9a48a3a01c91d275679e425e47f995950221674
Devin Beu <devinbeu@gmail.com>
2025-03-21 14:23:43 -0700

    coming in hot

---
+++ index.js
@@ -0,0 +1,103 @@
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+const path = require("path");
+app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.json());
+
+app.use(morgan("tiny"));
+
+let persons = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+app.use(requestLogger);
+
+const min = 1;
+const max = 100000000;
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = req.params.id; // Convert id to a number
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  persons = persons.filter((person) => person.id !== id);
+  res.status(204).end();
+});
+
+app.post("/api/persons", (req, res) => {
+  const body = req.body;
+  if (!body.name || !body.number) {
+    return res.status(400).json({ error: "name or number missing" });
+  }
+  if (
+    persons.find(
+      (person) => person.name.toLowerCase() === body.name.toLowerCase()
+    )
+  ) {
+    return res.status(400).json({ error: "name must be unique" });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: getRandomInt(min, max),
+  };
+  persons = persons.concat(person);
+  res.json(person);
+});
+
+app.get("/api/info", (req, res) => {
+  const length = persons.length;
+  res.send(`<h2>Phonebook currently has info for ${length} people.</h2>`);
+});
+
+const PORT = 3002;
+app.listen(PORT, () => {
+  console.log(`server is running on ${PORT}`);
+});
